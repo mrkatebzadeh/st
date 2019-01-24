7bf9fa66876ba7321d6810cf8cb00708c3613835
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-01-24 03:41:26 +0000

     Changed cursor

--- config.h
+++ config.h
@@ -130,7 +130,7 @@ static unsigned int defaultrcs = 4;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 6;
+static unsigned int cursorshape = 2;
 
 /*
  * Default columns and rows numbers
