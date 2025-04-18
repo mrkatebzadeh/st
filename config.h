3328648d67dbac1daad6dea1d2e68414d02f8c26
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-04-18 21:46:25 +0000

    Update bg

--- config.h
+++ config.h
@@ -133,7 +133,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
+	"#292f2f", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
 };
 
